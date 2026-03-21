2d69613b7343b3c24e57235b301346cef7b8cf68
prasadrprabhu77 <prasadrprabhu77@gmail.com>
2026-03-21 23:20:37 +0530

    task model is added

---
+++ Routes/taskRoute.js
@@ -0,0 +1,7 @@
+import express from "express"
+
+
+const taskRouter = express.Router();
+
+
+export default taskRouter;
---
+++ model/task.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      trim: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["pending", "in-progress", "completed"],
+      default: "pending",
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // relation with User model
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
