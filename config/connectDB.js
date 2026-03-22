b6a7b6bfca2b3cb8da3205c74cdc535fec5eae40
prasadrprabhu77 <prasadrprabhu77@gmail.com>
2026-03-22 10:35:36 +0530

    mogoDB atlas added

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose"
-
+import dotenv from "dotenv"
+dotenv.config();
 const connectDB = async() =>{
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/TasksDB")
+        await mongoose.connect(process.env.MONGO_URI)
+        // console.log(process.env.MONGO_URI)
         console.log("DB Connected sucessfully.")
     } catch (error) {
         console.log({"Error": error.message})
